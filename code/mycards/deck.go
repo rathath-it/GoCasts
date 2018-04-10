b60cc3b7ec867bf4f168fdcb3e2441d64ea3f643
abdennour <mail@abdennoor.com>
2018-04-10 22:09:28 +0300

    Build factory of cards

--- code/mycards/deck.go
+++ code/mycards/deck.go
@@ -7,6 +7,19 @@ import (
 // Create new type of 'deck' as slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Banana", "Faroula", "Toffaha"}
+	cardValues := []string{"Two", "Three", "Four"}
+	for i, suit := range cardSuits {
+		for j, val := range cardValues {
+			fmt.Println(i, j)
+			cards = append(cards, suit+" "+val)
+		}
+	}
+	return cards
+}
+
 // receiver : setup functions to variables type that was created
 // "d" because it is the beginning of the type "deck".. one or two letters
 func (d deck) print() {
