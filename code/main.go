eacb72166bf27fa51dd28281937836468811792c
abdennour <mail@abdennoor.com>
2018-04-10 20:42:58 +0300

    Initialize/assign variable in go

--- code/main.go
+++ code/main.go
@@ -1,10 +1,9 @@
-package main
-
 // It means it is package executable and not reusable
+package main
 
 import "fmt"
 
 // Since package main, main function should exist
 func main() {
-	fmt.Println("Hi there")
+	fmt.Println("Hi there and here")
 }
---
+++ code/mycards/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//var card string = "ACe DFd"
+	// : means figure out what is the type of var
+	// := it is initialization and not only asssignment
+	card := "Ace Hello"
+	card = "Ace updated"
+	fmt.Println(card)
+}
