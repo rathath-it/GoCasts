0dc11330f205e2616f8b0396d04d6e2db9d024a9
abdennour <mail@abdennoor.com>
2018-04-10 18:47:48 +0300

    Hello World Go lang

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # GoCasts
 
 Companion repo to a course on Udemy.com
+
+
+# Go to environment:
+
+```
+docker run -v $(pwd):/tmp -it golang:latest sh
+```
---
+++ code/main.go
@@ -0,0 +1,10 @@
+package main
+
+// It means it is package executable and not reusable
+
+import "fmt"
+
+// Since package main, main function should exist
+func main() {
+	fmt.Println("Hi there")
+}
