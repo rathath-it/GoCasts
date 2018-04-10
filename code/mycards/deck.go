6cd4e7799a3e591df8e2f8337315926f0069ae64
abdennour <mail@abdennoor.com>
2018-04-10 21:50:15 +0300

    docs: Receiver, naming,

--- code/mycards/deck.go
+++ code/mycards/deck.go
@@ -7,6 +7,8 @@ import (
 // Create new type of 'deck' as slice of strings
 type deck []string
 
+// receiver : setup functions to variables type that was created
+// "d" because it is the beginning of the type "deck".. one or two letters
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
