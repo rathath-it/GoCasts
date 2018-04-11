d6194a75b1d1883a4353d2265f48478d63fa8bd3
abdennour <mail@abdennoor.com>
2018-04-12 02:14:20 +0300

    docs: More doc about pointer

--- code/school/main.go
+++ code/school/main.go
@@ -5,9 +5,12 @@ import (
 )
 
 func main() {
+	// ahmad is a reference to a block (address) in the memory
 	ahmad := getStudent("Ahmad")
 	fmt.Println(ahmad.toString())
+	// &var means give me the memory address of the value this var is pointing at
 	pointerToAhmed := &ahmad
+	fmt.Printf("pointer value ", *pointerToAhmed)
 	pointerToAhmed.updateName("Mr Ahmed")
 	fmt.Println(ahmad.toString())
 }
--- code/school/model.go
+++ code/school/model.go
@@ -28,7 +28,9 @@ func (s student) toString() string {
 	return s.firstname + " " + s.lastname + ", living: " + s.address.street
 }
 
+// But (pointer+type) means a pointer of that type
 func (pointerToStudent *student) updateName(firstname string) {
 	// Change in the original memory and not a copy of the object
+	// *pointer means: Give me the value this memory address is pointing at
 	(*pointerToStudent).firstname = firstname
 }
